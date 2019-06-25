31c3f0115f21702762a4fa7b475feae0e661232e
Mustafa Masvi <mustafamasvi786@gmail.com>
2019-06-25 23:34:40 +0530

    Add one-page portfolio

---
+++ config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  siteTitle: 'Shivam Bhatt', // <title>
+  manifestName: 'Shivam',
+  manifestShortName: 'Landing', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/images/avatar.png',
+  pathPrefix: '/', // This path is subpath of your hosting https://domain/portfolio
+  authorName: 'Shivam Bhatt',
+  heading: 'Full Stack Developer',
+  // social
+  socialLinks: [
+    {
+      icon: 'fa-linkedin',
+      name: 'Linkedin',
+      url: 'https://www.linkedin.com/in/shivam-bhatt-041b45107/',
+    },
+    {
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/shivambhatt',
+    },
+    {
+      icon: 'fa-instagram',
+      name: 'Instagram',
+      url: 'https://twitter.com/5hivam_bhatt',
+    },
+    {
+      icon: 'fa-facebook',
+      name: 'Facebook',
+      url: 'https://facebook.com/shivam.bhatt.378',
+    },
+    // {
+    //   icon: 'fa-envelope-o',
+    //   name: 'Email',
+    //   url: 'mailto:shivambhatt42868@gmail.com',
+    // },
+  ],
+};
